83a3e7fc69954ebb4141dc388a7ad89d741a9ef4
Morthlog <iosifpetroulakis@hotmail.com>
2024-12-16 13:52:36 +0200

    fix: grammar following wrong syntax rules

--- minipythonexample.py
+++ minipythonexample.py
@@ -9,5 +9,67 @@ def fib(n):    # write Fibonacci series up to n
                         a = b
                         b = a + b
                       
-def funcwithdef(name,university="aueb"):
+def funcwithdef(name,university="aueb", ok, val = 5):
        print name, " studies in ", university
+
+def statements():
+       if true:
+              while false:
+                     for a in b:
+                            return a
+                            print a , b
+                     a = 55
+                     b -= 66
+              c /= 77
+              a [a] = b
+              assert f , d
+       B()
+       C(88 , 3, s)
+
+import math.plus
+import math.div as libDiv
+import math.mult as libMult, myLib as call
+
+def comparisons():
+       if not false:
+              a=b
+       if true and false:
+              a=b
+       if false or true:
+              a=b
+       if not 5 > 6:
+              a=b
+       if a < b and c < a:
+              a=b
+       if 66 < 77 or 88 == a:
+              a=b 
+
+def value(val1 = a.func(), val2 = 8, val3= " ",val4= ' sas ',val5= None):
+        return None
+
+def expressions():
+       print a
+       print a + 5
+       print a - 5
+       print a * 5
+       print a / 5
+       print 10 % 4
+       print 2 ** 3
+       print 1 + 2 - 3 +5 *7/7%10 - 1 ** 2
+       print a[8]
+       print A("string1")
+       print A.B("string2")
+       print 10
+       print len (5)
+       print ascii (b)
+       print max (6, A.func("val", "val2"), "pop", None)
+       print max (A.func('val', 'val2'), 'pop')
+       print min (3)
+       print (5)
+       print [4, "val"]
+
+def comparisonOrder():
+       return None
+
+def arithmeticOrder():
+       return None
